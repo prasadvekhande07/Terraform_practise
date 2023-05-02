c0a9aa547ccad632868e6348f5c0b53a1c734d58
prasadvekhande07 <96009907+prasadvekhande07@users.noreply.github.com>
2023-05-03 01:05:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  profile = "myaws"
+  region  = ap-south-1
+}
+
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "myawsbucket12345554187"
+  acl    = "public"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
